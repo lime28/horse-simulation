1fd43feb20e79b10cf8cfa0fdea6a1095555dcb9
Liam Habiby <liam@Liams-MacBook-Pro.local>
2026-07-22 14:12:23 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { LineMaterial } from 'three/addons/lines/LineMaterial.js'
 import { LineSegments2 } from 'three/addons/lines/LineSegments2.js'
 import { LineSegmentsGeometry } from 'three/addons/lines/LineSegmentsGeometry.js'
-import horseMetadata from '../../horse_metadata.json'
+import horseMetadata from './data/horse_metadata.json'
 import './App.css'
 
 const races = {
